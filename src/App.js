728710dde3c38065927463126f66deaa81424ec4
Muhammad Baqir <muhammadbaqirjafari@gmail.com>
2020-06-22 19:12:27 +0500

    Complete App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import {IncomeExpenses} from './components/IncomeExpenses';
+import {TransactionLists} from './components/TransactionLists';
+import {AddTransaction} from './components/AddTransaction';
+
+import {GlobalProvider} from './context/GlobalState';
 
 function App() {
   return (
-    <div>
-      Hello World!  
-      
-    </div>
+    <GlobalProvider>
+      <Header/>
+      <div className="container">
+           <Balance/>
+           <IncomeExpenses/>
+           <TransactionLists/>
+           <AddTransaction/>
+      </div>
+    </GlobalProvider>
   );
 }
 
